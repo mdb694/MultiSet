3e93e9363d082c15262686bc56e42cab9a7f7b21
mdb694 <mdb69@hotmail.it>
2016-04-09 18:26:30 +0200

    Implementata la classe template dell'elemento base <elemento, occorrenze>

--- pair.h
+++ pair.h
@@ -1,6 +1,8 @@
 #ifndef PAIR_H
 #define PAIR_H
 
+#include <algorithm>
+
 template<typename T> 
 class pair {
 	public:
@@ -13,44 +15,70 @@ class pair {
 	public:
 
 	/** Costruttore di default **/ 
- pair(void);
+ pair() : _element(0), _occourrence(0) {
+
+	}
 	
 	/** Costruttore secondario 
 			Crea una coppia con un numero specificato di elementi
 			@param elem elemento della coppia
 			@param occ numero di occorrenze dell'elemento 
 	**/
-	pair(const T &elem, size_type occ);
+	pair(const T &elem, size_type occ) : _element(elem), _occourrence(occ) {
+
+	}
 
 	/** Costruttore secondario
 			Crea una coppia con una sola occorrenza dell'elemento
 			@param elem elemento della coppia
 	**/
-	pair(const T &elem);
+	pair(const T &elem) : _element(elem), _occourrence(1) {
+
+	}
 
 	/** Distruttore **/
-	~pair(void);
+	~pair() {
+		_element = 0;
+		_occourrence = 0;
+	}
 
 	/** Copy constructor 
 			@param other pair da copiare
 	**/
-	pair(const pair& other);
+	pair(const pair& other) : _element(other._element), _occourrence(other._occourrence) {
+
+	}
 
 	/** Operatore di assegnamento
 			@param other pair da cui effettuare la copiare
 	**/
-	pair& operator=(const pair &other);
+	pair& operator=(const pair &other) {
+		if (this != &other) {
+			pair tmp(other);
+		
+			std::swap(_element, tmp._element);
+			std::swap(_occourrence, tmp._occourrence);
+		}
+
+		return *this;
+	}
 
 	/** Metodo getter dell'elemento
 	**/
-	T &element(void);
+	T &element() {
+		return _element;
+	}
 
 	/** Metode getter costante dell'elemento
 	**/
-	const T &element(void) const;
+	const T &element() const {
+		return _element;
+	}
 
 	/** Metodo getter delle occorrenze
 	**/
-	size_type occourrence(void);
-}
+	size_type occourrence() {
+		return _occourrence;
+	}
+}; //fine classe
 #endif 
